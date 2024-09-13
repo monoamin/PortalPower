13e5351d1a1b1d97fec1a2528d366a58835c3d9d
ppignet <p.pignet@think-modular.com>
2024-09-13 07:07:12 +0200

    Add config file

--- src/main/java/net/monoamin/daylight/DayLight.java
+++ src/main/java/net/monoamin/daylight/DayLight.java
@@ -1,6 +1,12 @@
 package net.monoamin.daylight;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.config.ModConfigEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
 @Mod(DayLight.MODID)
 public class DayLight {
     public static final String MODID = "daylight";
@@ -9,5 +15,18 @@ public class DayLight {
     public DayLight() {
         MinecraftForge.EVENT_BUS.register(this);
         MinecraftForge.EVENT_BUS.register(DayLightEvents.class);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onLoad);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        net.minecraftforge.common.ForgeConfigSpec commonSpec = DayLightConfig.COMMON_CONFIG;
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, commonSpec);
+    }
+
+    private void onLoad(final ModConfigEvent event) {
+        if (event.getConfig().getSpec() == DayLightConfig.COMMON_CONFIG) {
+            // React to changes or reload options if necessary
+        }
     }
 }
--- src/main/java/net/monoamin/daylight/DayLightEvents.java
+++ src/main/java/net/monoamin/daylight/DayLightEvents.java
@@ -12,7 +12,6 @@ import net.lavabucket.hourglass.config.HourglassConfig;
 @Mod.EventBusSubscriber(modid = DayLight.MODID)
 public class DayLightEvents {
     private static MinecraftServer server;
-    private static final double b = 0.5;
     @SubscribeEvent
     public static void onServerStarting(ServerStartingEvent event) {
         server = event.getServer();
@@ -24,18 +23,49 @@ public class DayLightEvents {
             int s = ((Season.SubSeason)subSeason).ordinal();
             if (DayLight.DEBUG)
                 LogManager.getLogger().debug("####DAYLIGHT####:  Season changed to " + s);
-            if (s == 0) setDayNightSpeed(0.75 * b, 1.25 * b);
-            if (s == 1) setDayNightSpeed(1.00 * b, 1.00 * b);
-            if (s == 2) setDayNightSpeed(1.25 * b, 0.75 * b);
-            if (s == 3) setDayNightSpeed(1.40 * b, 0.60 * b);
-            if (s == 4) setDayNightSpeed(1.50 * b, 0.75 * b);
-            if (s == 5) setDayNightSpeed(1.40 * b, 0.60 * b);
-            if (s == 6) setDayNightSpeed(1.25 * b, 0.75 * b);
-            if (s == 7) setDayNightSpeed(1.00 * b, 1.00 * b);
-            if (s == 8) setDayNightSpeed(0.75 * b, 1.25 * b);
-            if (s == 9) setDayNightSpeed(0.60 * b, 1.40 * b);
-            if (s == 10) setDayNightSpeed(0.50 * b, 1.50 * b);
-            if (s == 11) setDayNightSpeed(0.60 * b, 1.40 * b);
+            double dayFactor = 1.0;
+            double nightFactor = 1.0;
+
+            switch (s){
+                case 0:
+                    dayFactor = DayLightConfig.ratioEarlySpring.get();
+                    nightFactor = 2-DayLightConfig.ratioEarlySpring.get();
+                case 1:
+                    dayFactor = DayLightConfig.ratioMidSpring.get();
+                    nightFactor = 2-DayLightConfig.ratioMidSpring.get();
+                case 2:
+                    dayFactor = DayLightConfig.ratioLateSpring.get();
+                    nightFactor = 2-DayLightConfig.ratioLateSpring.get();
+                case 3:
+                    dayFactor = DayLightConfig.ratioEarlySummer.get();
+                    nightFactor = 2-DayLightConfig.ratioEarlySummer.get();
+                case 4:
+                    dayFactor = DayLightConfig.ratioMidSummer.get();
+                    nightFactor = 2-DayLightConfig.ratioMidSummer.get();
+                case 5:
+                    dayFactor = DayLightConfig.ratioLateSummer.get();
+                    nightFactor = 2-DayLightConfig.ratioLateSummer.get();
+                case 6:
+                    dayFactor = DayLightConfig.ratioEarlyFall.get();
+                    nightFactor = 2-DayLightConfig.ratioEarlyFall.get();
+                case 7:
+                    dayFactor = DayLightConfig.ratioMidFall.get();
+                    nightFactor = 2-DayLightConfig.ratioMidFall.get();
+                case 8:
+                    dayFactor = DayLightConfig.ratioLateFall.get();
+                    nightFactor = 2-DayLightConfig.ratioLateFall.get();
+                case 9:
+                    dayFactor = DayLightConfig.ratioEarlyWinter.get();
+                    nightFactor = 2-DayLightConfig.ratioEarlyWinter.get();
+                case 10:
+                    dayFactor = DayLightConfig.ratioMidWinter.get();
+                    nightFactor = 2-DayLightConfig.ratioMidWinter.get();
+                case 11:
+                    dayFactor = DayLightConfig.ratioLateWinter.get();
+                    nightFactor = 2-DayLightConfig.ratioLateWinter.get();
+            }
+            double overallScaling = DayLightConfig.overallScaling.get();
+            if (s == 0) setDayNightSpeed(dayFactor * overallScaling, nightFactor * overallScaling);
         }
     }
     private static void setDayNightSpeed(double dayFactor, double nightFactor) {
